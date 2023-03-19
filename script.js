d2cca127ff44153ef8fd1b37dcb61ce5f5420306
Humberto Alfaro <humbertoalfaro@Humbertos-MacBook-Pro.local>
2023-03-18 20:09:47 -0400

    added current date, tried to create a function to match the color with the current hour

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// Displays the date at the top of the page 
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
+
+function currentHour() {
+
+  // this tells me what time it currently is
+  var currentTime = today.hour()
+  console.log('The current hour is ' + currentTime);
+
+
+  // grabs the hour- id and adds current time 
+  var divhours = $('#hour-' + currentTime)
+  console.log(divhours)
+
+ var past = document.getElementsByClassName('past')
+ var present = document.getElementsByClassName('present')
+ var future = document.getElementsByClassName('future')
+  
+ 
+
+
+  // if(divhours.length < currentTime) {
+  //   $('.time-block').css('background-color', 'red')
+  //   console.log ('This will be red for present')
+  // }else if(currentTime < divhours) {
+    
+  // }else if(currentTime > divhours){
+    
+  // }else(console.log('error'))
+
+  // creates a for loop to represent
+  // for (var hour = 0; hour < 24; hour++) {
+  //   var divhours = $('<div>').attr('id', 'hour-' + currentTime);
+  // }
+
+ 
+}
+
+
+currentHour()
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
